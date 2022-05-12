f9faf91634843e78798240f69c3f2a4d1e3ec90d
Federico Iglesias <figlesias221@gmail.com>
2022-05-12 12:15:43 -0300

    arreglo de parseo de las fechas

--- Material Obligatorio/Obligatorio/MinTurFrontend/src/app/feature-modules/explore-one-resort/make-reservation/make-reservation.component.ts
+++ Material Obligatorio/Obligatorio/MinTurFrontend/src/app/feature-modules/explore-one-resort/make-reservation/make-reservation.component.ts
@@ -9,7 +9,7 @@ import { ReservationConfirmationModel } from 'src/app/shared/models/out/reservat
 @Component({
   selector: 'app-make-reservation',
   templateUrl: './make-reservation.component.html',
-  styleUrls: []
+  styleUrls: [],
 })
 export class MakeReservationComponent implements OnInit {
   @Input() accommodationDetails: AccommodationIntentModel;
@@ -24,52 +24,53 @@ export class MakeReservationComponent implements OnInit {
   public reservationConfirmation: ReservationConfirmationModel;
   private reservationIntent: ReservationIntentModel;
 
-  constructor(private reservationService: ReservationService) { }
+  constructor(private reservationService: ReservationService) {}
 
-  ngOnInit(): void{
-  }
+  ngOnInit(): void {}
 
-  public setName(name: string): void{
+  public setName(name: string): void {
     this.name = name;
   }
 
-  public setSurname(surname: string): void{
+  public setSurname(surname: string): void {
     this.surname = surname;
   }
 
-  public setEmail(email: string): void{
+  public setEmail(email: string): void {
     this.email = email;
   }
 
-  public makeReservation(): void{
+  public makeReservation(): void {
     this.validateFields();
-    if (!this.displayError){
+    if (!this.displayError) {
       this.constructReservationIntent();
 
-      this.reservationService.makeReservation(this.reservationIntent).subscribe(confirmation => this.loadConfirmation(confirmation),
-        (error: HttpErrorResponse) => this.showError(error));
+      this.reservationService.makeReservation(this.reservationIntent).subscribe(
+        (confirmation) => this.loadConfirmation(confirmation),
+        (error: HttpErrorResponse) => this.showError(error)
+      );
     }
   }
 
-  public closeError(): void{
+  public closeError(): void {
     this.displayError = false;
     this.errorMessages = [];
   }
 
-  public parseDateIntoMDY(date: Date): string{
+  public parseDateIntoMDY(date: Date): string {
     const month = date.getMonth() + 1;
-    const day = date.getDay();
+    const day = date.getDate();
     const year = date.getFullYear();
 
     return month + '/' + day + '/' + year;
   }
 
-  private loadConfirmation(confirmation: ReservationConfirmationModel): void{
+  private loadConfirmation(confirmation: ReservationConfirmationModel): void {
     this.reservationConfirmation = confirmation;
     this.justMadeReservation = true;
   }
 
-  private constructReservationIntent(): void{
+  private constructReservationIntent(): void {
     this.reservationIntent = {
       resortId: this.resortId,
       name: this.name,
@@ -80,43 +81,49 @@ export class MakeReservationComponent implements OnInit {
       adultsAmount: this.accommodationDetails.adultsAmount,
       kidsAmount: this.accommodationDetails.kidsAmount,
       babiesAmount: this.accommodationDetails.babiesAmount,
-      retiredAmount: this.accommodationDetails.retiredAmount
+      retiredAmount: this.accommodationDetails.retiredAmount,
     };
   }
 
-  private validateFields(): void{
+  private validateFields(): void {
     this.closeError();
     this.validateName();
     this.validateSurname();
     this.validateEmail();
   }
 
-  private validateName(): void{
+  private validateName(): void {
     const regex = new RegExp('^[a-zA-ZñÑáéíóúü ]+$');
 
-    if (this.name == null || !regex.test(this.name)){
+    if (this.name == null || !regex.test(this.name)) {
       this.displayError = true;
-      this.errorMessages.push('El nombre ingresado es invalido - solo caracteres alfabeticos');
+      this.errorMessages.push(
+        'El nombre ingresado es invalido - solo caracteres alfabeticos'
+      );
     }
   }
 
-  private validateSurname(): void{
+  private validateSurname(): void {
     const regex = new RegExp('^[a-zA-ZñÑáéíóúü ]+$');
 
-    if (this.surname == null || !regex.test(this.surname)){
+    if (this.surname == null || !regex.test(this.surname)) {
       this.displayError = true;
-      this.errorMessages.push('El apellido ingresado es invalido - solo caracteres alfabeticos');
+      this.errorMessages.push(
+        'El apellido ingresado es invalido - solo caracteres alfabeticos'
+      );
     }
   }
 
-  private validateEmail(): void{
-    if (this.email == null || !isEmailValid(this.email)){
+  private validateEmail(): void {
+    if (this.email == null || !isEmailValid(this.email)) {
       this.displayError = true;
-      this.errorMessages.push('Es necesario especificar un email y que este sea válido');
+      this.errorMessages.push(
+        'Es necesario especificar un email y que este sea válido'
+      );
     }
   }
 
-  private showError(error: HttpErrorResponse): void{
+  private showError(error: HttpErrorResponse): void {
     console.log(error);
   }
 }
