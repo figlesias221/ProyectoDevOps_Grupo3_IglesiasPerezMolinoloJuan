514535d00beb6df3ce67e86d22e9d274dc0a4016
andrujuanoo <51160185+andrujuanoo@users.noreply.github.com>
2022-05-21 11:44:05 -0300

    TDD sobre bug de calculador de precios de reservas (#77)

--- Material Obligatorio/Obligatorio/Ministerio de Turismo/MinTur.BusinessLogic.Test/Pricing/ResortPricingCalculatorTest.cs
+++ Material Obligatorio/Obligatorio/Ministerio de Turismo/MinTur.BusinessLogic.Test/Pricing/ResortPricingCalculatorTest.cs
@@ -2,6 +2,9 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MinTur.BusinessLogic.Pricing;
 using System;
+using System.Collections.Generic;
+using MinTur.Domain.DiscountPolicies.GuestGroups;
+using Moq;
 
 namespace MinTur.BusinessLogic.Test.Pricing
 {
@@ -9,104 +12,70 @@ namespace MinTur.BusinessLogic.Test.Pricing
     public class ResortPricingCalculatorTest
     {
         private ResortPricingCalculator _resortPricingCalculator;
-        private const int KidsDiscount = 2;
-        private const int BabiesDiscount = 4;
+        private List<IGuestGroupDiscountPolicy> _guestGroupDiscounts;
+        private Mock<GuestGroup> _mockGuestGroup;
+        private Resort _resort;
+        private int ResortPricePerNight = 126;
+        private int ResortId = 1;
+        private String ResortName = "Hotel Italiano";
+        private readonly DateTime _accommodationCheckInSameTime = new DateTime(2020, 9, 27);
+        private readonly DateTime _accommodationCheckOutSameTime = new DateTime(2020, 9, 30);
+        private readonly DateTime _accommodationCheckInDifferentTime = new DateTime(2020, 9, 27, 12, 0, 0);
+        private readonly DateTime _accommodationCheckOutDifferentTime = new DateTime(2020, 9, 30, 16, 0, 0);
+        private int GuestGroupAmount = 2;
 
         [TestInitialize]
         public void SetUp() 
         {
+            _mockGuestGroup = new Mock<GuestGroup>(MockBehavior.Strict);
+            _guestGroupDiscounts = new List<IGuestGroupDiscountPolicy>();
+            
+            _resort = new Resort()
+            {
+                Id = ResortId,
+                Name = ResortName,
+                PricePerNight = ResortPricePerNight
+            };
+
             _resortPricingCalculator = new ResortPricingCalculator();
         }
 
         [TestMethod]
-        public void TotalPriceForAccommodationCalculatedCorrectly1() 
+        public void TotalPriceForAccommodationCalculatedCorrectlyWithMajorDiscount()
         {
-            Resort resort = new Resort()
-            {
-                Id = 1,
-                Name = "Hotel Italiano",
-                PricePerNight = 126
-            };
+            _guestGroupDiscounts.Add(new BabiesDiscountPolicy());
+            _guestGroupDiscounts.Add(new KidsDiscountPolicy());
+            _mockGuestGroup.Setup(x => x.GetApplicableDiscountPolicies()).Returns(_guestGroupDiscounts);
+            _mockGuestGroup.Setup(x => x.Amount).Returns(GuestGroupAmount);
+            
             Accommodation accommodation = new Accommodation()
             {
-                CheckIn = new DateTime(2020, 9, 27),
-                CheckOut = new DateTime(2020, 9, 30)
+                CheckIn = _accommodationCheckInSameTime,
+                CheckOut = _accommodationCheckOutSameTime
             };
-            accommodation.Guests.Add(new GuestGroup() { GuestGroupType = GuestType.Adult.ToString(), Amount = 2 });
-            accommodation.Guests.Add(new GuestGroup() { GuestGroupType = GuestType.Kid.ToString(), Amount = 4 });
-            accommodation.Guests.Add(new GuestGroup() { GuestGroupType = GuestType.Baby.ToString(), Amount = 3 });
-            accommodation.Guests.Add(new GuestGroup() { GuestGroupType = GuestType.Retired.ToString(), Amount = 3 });
+            accommodation.Guests.Add((_mockGuestGroup.Object));
 
-            int expectedPrice = 2664;
-            int totalPrice = _resortPricingCalculator.CalculateTotalPriceForAccommodation(resort, accommodation);
+            double majorPolicyDiscount = new KidsDiscountPolicy().GetAssociatedDiscount();
+            int amountOfNightsInAccommodation = 3;
+            double expectedPrice = amountOfNightsInAccommodation * ResortPricePerNight * GuestGroupAmount * majorPolicyDiscount;
+            int totalPrice = _resortPricingCalculator.CalculateTotalPriceForAccommodation(_resort, accommodation);
             Assert.AreEqual(expectedPrice, totalPrice);
         }
 
         [TestMethod]
-        public void TotalPriceForAccommodationCalculatedCorrectly2()
+        public void TotalPriceForAccommodationWithLaterCheckOutTimeThanCheckInTime()
         {
-            Resort resort = new Resort()
-            {
-                Id = 1,
-                Name = "Hotel Americano",
-                PricePerNight = 200
-            };
             Accommodation accommodation = new Accommodation()
             {
-                CheckIn = new DateTime(2020, 9, 27),
-                CheckOut = new DateTime(2020, 10, 7)
+                CheckIn = _accommodationCheckInDifferentTime,
+                CheckOut = _accommodationCheckOutDifferentTime
             };
-            accommodation.Guests.Add(new GuestGroup() { GuestGroupType = GuestType.Adult.ToString(), Amount = 4 });
-            accommodation.Guests.Add(new GuestGroup() { GuestGroupType = GuestType.Kid.ToString(), Amount = 2 });
+            accommodation.Guests.Add(new GuestGroup() { GuestGroupType = GuestType.Adult.ToString(), Amount = GuestGroupAmount});
 
-            int expectedPrice = 10000;
-            int totalPrice = _resortPricingCalculator.CalculateTotalPriceForAccommodation(resort, accommodation);
+            int amountOfNightsInAccommodation = 3;
+            double expectedPrice =   amountOfNightsInAccommodation * ResortPricePerNight * GuestGroupAmount;
+            int totalPrice = _resortPricingCalculator.CalculateTotalPriceForAccommodation(_resort, accommodation);
             Assert.AreEqual(expectedPrice, totalPrice);
         }
-
-        [TestMethod]
-        public void TotalPriceForAccommodationCalculatedCorrectly3()
-        {
-            Resort resort = new Resort()
-            {
-                Id = 1,
-                Name = "Hotel Aleman",
-                PricePerNight = 187
-            };
-            Accommodation accommodation = new Accommodation()
-            {
-                CheckIn = new DateTime(2020, 9, 27),
-                CheckOut = new DateTime(2020, 9, 29)
-            };
-            accommodation.Guests.Add(new GuestGroup() { GuestGroupType = GuestType.Adult.ToString(), Amount = 4 });
-            accommodation.Guests.Add(new GuestGroup() { GuestGroupType = GuestType.Baby.ToString(), Amount = 2 });
-
-            int expectedPrice = 1683;
-            int totalPrice = _resortPricingCalculator.CalculateTotalPriceForAccommodation(resort, accommodation);
-            Assert.AreEqual(expectedPrice, totalPrice);
-        }
-
-        [TestMethod]
-        public void TotalPriceForAccommodationCalculatedCorrectly4()
-        {
-            Resort resort = new Resort()
-            {
-                Id = 1,
-                Name = "Hotel Ingles",
-                PricePerNight = 364
-            };
-            Accommodation accommodation = new Accommodation()
-            {
-                CheckIn = new DateTime(2020, 9, 27),
-                CheckOut = new DateTime(2020, 9, 28)
-            };
-            accommodation.Guests.Add(new GuestGroup() { GuestGroupType = GuestType.Adult.ToString(), Amount = 2 });
-            accommodation.Guests.Add(new GuestGroup() { GuestGroupType = GuestType.Retired.ToString(), Amount = 2 });
-
-            int expectedPrice = 1201;
-            int totalPrice = _resortPricingCalculator.CalculateTotalPriceForAccommodation(resort, accommodation);
-            Assert.AreEqual(expectedPrice, totalPrice);
-        }
-
     }
 }
