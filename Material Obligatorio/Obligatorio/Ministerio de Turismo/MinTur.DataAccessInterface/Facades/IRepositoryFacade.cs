78ffd3659630abe66c0a2c580e0d45f5a6fd8478
Matias Molinolo <mm231323@fi365.ort.edu.uy>
2022-05-22 17:41:00 -0300

    fix conflicts

--- Material Obligatorio/Obligatorio/Ministerio de Turismo/MinTur.DataAccessInterface/Facades/IRepositoryFacade.cs
+++ Material Obligatorio/Obligatorio/Ministerio de Turismo/MinTur.DataAccessInterface/Facades/IRepositoryFacade.cs
@@ -2,11 +2,7 @@
 
 namespace MinTur.DataAccessInterface.Facades
 {
-<<<<<<< HEAD
     public interface IRepositoryFacade : IRegionRepository, ITouristPointRepository, IChargingPointRepository, ICategoryRepository, IResortRepository, IReservationRepository,
-=======
-    public interface IRepositoryFacade : IRegionRepository, ITouristPointRepository, ICategoryRepository, IResortRepository, IReservationRepository,
->>>>>>> main
         IAuthenticationTokenRepository, IAdministratorRepository, IReviewRepository
     {
 
